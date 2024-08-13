9916e1cc62d47fa9727a3a018be5f8abcc5f8167
kanuki <kalevi.toivanen@gmail.com>
2024-08-13 20:23:40 +0300

    improved opengl setup bash script

--- opengl/setup.sh
+++ opengl/setup.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/bash
 
-git clone git@github.com:glfw/glfw.git
-git clone https://github.com/nigels-com/glew.git
+if [ ! -d "glfw" ] | [ ! -d "glew" ]; then
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		git clone git@github.com:glfw/glfw.git
+		git clone git@github.com:nigels-com/glew.git
+		sudo apt-get install libglu1-mesa-dev
+		sudo apt-get install build-essential libxmu-dev libxi-dev libgl-dev
+		sudo apt install libwayland-dev libxkbcommon-dev xorg-dev
+		sudo apt install cmake
 
-sudo apt-get install libglu1-mesa-dev
-sudo apt-get install build-essential libxmu-dev libxi-dev libgl-dev
-sudo apt install libwayland-dev libxkbcommon-dev xorg-dev
-sudo apt install cmake
+		cd glew/auto
+		make
+		cd ..
+		make
 
-cd glew/auto
-make
-cd ..
-make
+		cd ../glfw
+		cmake -S . -B build
+		cd build
+		make
 
-cd ../glfw
-cmake -S . -B build
-cd build
-make
+	elif [[ "$OSTYPE" == "msys" ]]; then
+	git clone https://github.com/glfw/glfw.git
+			git clone https://github.com/nigels-com/glew.git
+	fi
+fi
+
+
 

03e814e96eeb413c9f55123da7e470dcff1cb235
kanuki00 <kalevi.toivanen@gmail.com>
2024-08-15 18:36:10 +0300

    vertex and fragment shaders

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -2,6 +2,32 @@
 #include "glfw/include/GLFW/glfw3.h"
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string &source) {
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	// specifying the source of our shader
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+	// TODO error handling
+	return id;
+}
+
+static unsigned int CreateShader(const std::string &vertexShaderSource, const std::string &fragmentShaderSource) {
+	unsigned int program = glCreateProgram();
+	unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderSource);
+	unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return program;
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -29,7 +55,7 @@ int main(void)
 	float positions[6] = {
 		-0.5f, -0.5f,
 		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.6f
 	};
 
 	unsigned int buffer;
@@ -40,17 +66,32 @@ int main(void)
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, static_cast<const void*>(0));
 
+	std::string vertexShader =
+		"#version 330 core\n"
+		"layout(location = 0) in vec4 position;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+	std::string fragmentShader =
+		"#version 330 core\n"
+		"layout(location = 0) out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 1.0, 0.0, 1.0);\n"
+		"}\n";
+
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -58,6 +99,8 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
